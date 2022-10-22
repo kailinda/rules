b45c1bc99b1fe444c7e85118e151f196f238fcd0
kailinda <2256876203@qq.com>
2022-10-22 22:59:34 +0800

    1. 修正smartdns启动逻辑

--- k2p/etc/storage/wan.d/dns/x-smartdns.sh
+++ k2p/etc/storage/wan.d/dns/x-smartdns.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 /sbin/start-stop-daemon -K -n smartdns >/dev/null 2>&1
 sleep 3
-/sbin/start-stop-daemon -S -c nobody -b -o -q -m -p /tmp/smartdns.pid -x /usr/bin/smartdns -- -c /etc/storage/smartdns/smartdns.conf && \
-logger -t "【SMART_DNS】" "smartdns启动,成功 ~~~" || \
-{ logger -t "【SMART_DNS】" "smartdns启动,失败 !!!";exit 1;}
+/sbin/start-stop-daemon -S -c nobody -b -o -q -m -p /tmp/smartdns.pid -x /usr/bin/smartdns -- -c /etc/storage/smartdns/smartdns.conf
+if [ $? -eq 0 ];then
+	logger -t "【SMART_DNS】" "smartdns启动,成功 ~~~"
+	sleep 10
+	logger -t "【SMART_DNS】" "清理smartdns日志 ~"
+	> /tmp/smartdns.log
+else
+	logger -t "【SMART_DNS】" "smartdns启动,失败 !!!"
+	exit 1
+fi
 exit 0
 
